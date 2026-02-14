28eef48ba6732ef353cf6b4afeefeb0fd7ce4033
Dulcie <vdulciemaria@gmail.com>
2026-02-14 12:23:55 +0800

    Initial commit

---
+++ hrllorits/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace hrllorits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ACCOUNT MANAGEMENT SYSTEM");
+
+            string username = "superuser";
+            string password = "superuser123";
+
+            Console.Write("Enter username: ");
+            string usernameInput = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string passwordInput = Console.ReadLine();
+
+            if (usernameInput == username && passwordInput == password)
+            {
+                Console.WriteLine("Login successful");
+            }
+            else
+            {
+                Console.WriteLine("Incoprrect credentials");
+            }
+        }
+    }
+}
